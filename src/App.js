1967ca9b94fc3c3ba3117f77a251027675a6664e
Thomas Kautenburger <tkautenburger@legendlime.de>
2020-11-21 20:17:24 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
+
 import DepartmentForm from './components/departmentForm';
 import EmployeeForm from './components/employeeForm';
 
-
 function App() {
   return (
-    <div className="app">
-      <DepartmentForm department={{ deptId: 4711, deptName: 'HR', deptDescription: 'Human Resources' }} />
-      <p /><p />
-      <EmployeeForm employee={{ empId: 101, lastName: 'Kautenburger', firstName: 'Thomas', deptId: 0 }} />
-    </div>
+    <Router>
+      <div className="app">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/employees" >
+                  <span>Employees</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/departments" >
+                  <span>Departments</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path="/departments">
+            <DepartmentForm department={{ deptId: 4711, deptName: 'HR', deptDescription: 'Human Resources' }} />
+          </Route>
+          <Route path="/employees">
+            <EmployeeForm employee={{ empId: 101, lastName: 'Kautenburger', firstName: 'Thomas', deptId: 0 }} />
+          </Route>
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
